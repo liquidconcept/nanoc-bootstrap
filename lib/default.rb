8050f55f4bdc12eda64fa92feb7b0c82ceb14636
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-06-06 16:46:53 +0200

    add common helpers

--- lib/default.rb
+++ lib/default.rb
@@ -1,7 +1,29 @@
 # encoding: utf-8
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
+
+# uncomment to use i18n filesystem
+# require 'nanoc3/data_sources/filesystem_i18n'
+
+def item
+  @item
+end
+
+def root
+  item_by_identifier(@item[:locale] ? "/#{@item[:locale]}/" : '/')
+end
+
+def root?(item = @item)
+  item.identifier == root.identifier
+end
 
 def item_id(item)
-  item.identifier.gsub(/^\/(.+)\/$/, '\1').gsub(/[\/_]+/, '-')
+  item.identifier == root?(item) ? 'home' : item.identifier.gsub(/^\/[a-z]{2}\/(.+)\/$/, '\1').gsub(/[\/_]+/, '-')
 end
+
+def item_by_identifier(identifier)
+  @items.find {|item| item.identifier == identifier }
+end
+
+def item_children(item)
+  item ? item.children.sort {|a, b| a[:position] <=> b[:position] } : []
+end
+
